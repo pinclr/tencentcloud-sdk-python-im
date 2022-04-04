573be67e0bad41eeb8393cac9b0508fed2499c1c
Robert Wen <robert.wen@nyu.edu>
2022-04-05 01:33:50 +0800

    update as IM and TCIM

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="tencentcloud-sdk-python-tim",
+    name="tencentcloud-sdk-python-im",
     version="0.0.1",
     author="Robert Wen",
     author_email="robert@pinclr.com",
     description="Python API Client for Tencent IM Product",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pinclr/tencentcloud-sdk-python-tim",
-    packages=['tencentcloud_tim'],
+    url="https://github.com/pinclr/tencentcloud-sdk-python-im",
+    packages=['tencentcloud_im'],
     package_dir={'': 'src'},
-    package_data={'': ['tim.json', '*.py']},
+    package_data={'': ['tcim.json', '*.py']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
