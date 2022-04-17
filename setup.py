d9bd6b5cb44d00c4abcdea42a6abcbbdbff2b248
Robert Wen <robert.wen@nyu.edu>
2022-04-17 21:04:15 +0800

    roll up to 0.9.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tencentcloud-sdk-python-im",
-    version="0.9.1",
+    version="0.9.2",
     author="Robert Wen",
     author_email="robert@pinclr.com",
     description="Python API Client for Tencent Cloud Instant Messaging Service",
